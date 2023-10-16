a5da8c77e9afd878324d773aad8c745bc58e6f34
Denis Boldyrev <laevus@yandex.ru>
2023-10-16 22:59:03 +0300

    init: start task

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
-      template: { transformAssetUrls }
+      template: { transformAssetUrls },
     }),
 
     quasar({
-      sassVariables: 'src/quasar-variables.sass'
-    })
-  ]
+      sassVariables: 'src/quasar-variables.sass',
+    }),
+  ],
 })
