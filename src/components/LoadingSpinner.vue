79456eb25b55685c3e06fa97e107ef5ba85bf5e1
Denis Boldyrev <laevus@yandex.ru>
2023-10-16 23:00:00 +0300

    feat: add loading spinner

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="q-pa-md">
+    <div class="fixed-center">
+      <q-spinner-hourglass color="primary"
+size="3em" />
+      <div class="text-overline">
+Loading
+</div>
+    </div>
+  </div>
+</template>
+<script>
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  name: 'LoadingSpinner',
+}
+</script>
+<style>
+.fixed-center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
