22b7e74e0fc574abd97e14255753c3ba30ba6e5f
Denis Boldyrev <laevus@yandex.ru>
2023-10-17 09:59:37 +0300

    feat: configure linter and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,65 @@
-const { configure, presets } = require('eslint-kit')
-
-module.exports = configure({
-  allowDebug: process.env.NODE_ENV !== 'production',
-
-  presets: [
-    presets.imports(),
-    presets.node(),
-    presets.prettier(),
-    presets.typescript(),
-    presets.vue({
-      version: 'detect',
-    }),
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    '@vue/typescript',
+    'plugin:prettier/recommended',
   ],
-
-  parser: 'vue-eslint-parser',
-})
+  rules: {
+    'vue/order-in-components': [
+      'error',
+      {
+        order: [
+          'name',
+          'directives',
+          'components',
+          'mixins',
+          ['provide', 'inject'],
+          'model',
+          'props',
+          'filters',
+          'data',
+          'computed',
+          'watch',
+          'methods',
+          'LIFECYCLE_HOOKS',
+          'ROUTER_GUARDS',
+        ],
+      },
+    ],
+    'vue/v-for-delimiter-style': ['error', 'of'],
+    'vue/next-tick-style': ['error', 'promise'],
+    'vue/require-prop-types': 'error',
+    'vue/prop-name-casing': ['error', 'camelCase'],
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/component-definition-name-casing': ['error', 'PascalCase'],
+    'vue/custom-event-name-casing': ['error', 'camelCase'],
+    'vue/no-duplicate-attr-inheritance': 'error',
+    'vue/this-in-template': ['error', 'never'],
+    'vue/v-on-style': ['error', 'shorthand'],
+    'vue/no-multi-spaces': 'error',
+    'vue/padding-line-between-blocks': 'error',
+    'vue/component-tags-order': [
+      'error',
+      {
+        order: ['template', 'script', 'style'],
+      },
+    ],
+    'vue/v-on-event-hyphenation': ['error', 'never'],
+    'vue/attribute-hyphenation': ['error', 'never'],
+    'vue/v-bind-style': 'error',
+    'vue/v-slot-style': ['error', 'shorthand'],
+    'vue/no-unused-properties': [
+      'error',
+      {
+        groups: ['props', 'data', 'computed', 'methods', 'setup'],
+        ignorePublicMembers: true,
+      },
+    ],
+  }
+}
